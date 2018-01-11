07ec2310928843ecdd67e6bb56890d0563d0d7ed
Mario Nebl <hello@mario-nebl.de>
2018-01-11 22:05:58 +0100

    fix(terminal): fall back to defaults if colors missing

--- src/parsers/terminal.test.js
+++ src/parsers/terminal.test.js
@@ -1,6 +1,7 @@
 const { terminal } = require("./terminal");
 const matchers = require("../matchers");
 const { atom, fixture, seti } = require("../helpers");
+const { terminal: {colors} } = require("terminal-default-colors");
 
 expect.extend(matchers);
 
@@ -30,29 +31,29 @@ test("works for valid input", async () => {
   expect(() => terminal(oneDark)).not.toThrow();
 });
 
-test("throws for incomplete input", async () => {
-  const incomplete = await fixture("terminal/incomplete.terminal");
-  expect(() => terminal(incomplete)).toThrow(/Missing ANSIBlackColor/);
+test("works for all known colorspaces", async () => {
+  const colorspaces = await fixture("terminal/colorspaces.terminal");
+  expect(() => terminal(colorspaces)).not.toThrow();
 });
 
 test("throws for malformed input", async () => {
   const malformed = await fixture("terminal/malformed.terminal");
   expect(() => terminal(malformed)).toThrow(
-    /Parsing ANSIBlackColor failed/
+    /Parsing black failed/
   );
 });
 
 test("throws for input with malformed keys", async () => {
   const malformed = await fixture("terminal/malformed-keys.terminal");
   expect(() => terminal(malformed)).toThrow(
-    /Parsing ANSIBlackColor failed/
+    /Parsing black failed/
   );
 });
 
 test("throws for input with malformed values", async () => {
   const malformed = await fixture("terminal/malformed-values.terminal");
   expect(() => terminal(malformed)).toThrow(
-    /Missing NSRGB in color ANSIBlackColor/
+    /Unknown NSColorSpace undefined in color black/
   );
 });
 
@@ -64,4 +65,21 @@ test("returns expected result for Seti", async () => {
 test("returns expected result for Atom", async () => {
   const data = await fixture("terminal/Atom.terminal");
   expect(terminal(data)).toEqual(atom);
+});
+
+test("returns expected result for incomplete input", async () => {
+  const incomplete = await fixture("terminal/incomplete.terminal");
+  const actual = terminal(incomplete);
+
+  expect(actual[0]).toEqual(colors[0].rgb);
+  expect(actual[1]).toEqual(colors[1].rgb);
+  expect(actual[2]).toEqual(colors[2].rgb);
+  expect(actual[3]).toEqual(colors[3].rgb);
+  expect(actual[4]).toEqual(colors[4].rgb);
+  expect(actual[5]).toEqual(colors[5].rgb);
+  expect(actual[6]).toEqual(colors[6].rgb);
+  expect(actual[7]).toEqual(colors[7].rgb);
+  expect(actual[8]).toEqual(colors[8].rgb);
+  expect(actual[9]).toEqual(colors[9].rgb);
+  expect(actual[10]).toEqual(colors[10].rgb);
 });
--- src/parsers/terminal.ts
+++ src/parsers/terminal.ts
@@ -1,9 +1,23 @@
+import * as util from "util";
 import is from "@marionebl/is";
 import { TermScheme, TermSchemeColor } from "./term-scheme";
 
 const AggregateError = require("aggregate-error");
 const { parse } = require("plist");
 const { parseBuffer } = require("bplist-parser");
+const {terminal: terminalDefaults} = require("terminal-default-colors");
+
+interface DefaultMap {
+  [name: string]: {rgb: TermSchemeColor};
+}
+
+const ADDITIONAL_DEFAULTS: DefaultMap = {
+  BackgroundColor: {rgb: [255, 255, 255]},
+  TextBoldColor: {rgb: [0, 0, 0]},
+  CursorColor: {rgb: [146, 146, 146]},
+  CursorTextColor: {rgb: [0, 0, 0]},
+  TextColor: {rgb: [0, 0, 0]}
+};
 
 interface TerminalData {
   ANSIBlackColor: Buffer;
@@ -45,22 +59,22 @@ export function terminal(input: any): TermScheme {
   }
 
   return {
-    0: toRGB(raw.ANSIBlackColor, "ANSIBlackColor"),
-    1: toRGB(raw.ANSIRedColor, "ANSIRedColor"),
-    2: toRGB(raw.ANSIGreenColor, "ANSIGreenColor"),
-    3: toRGB(raw.ANSIYellowColor, "ANSIYellowColor"),
-    4: toRGB(raw.ANSIBlueColor, "ANSIBlueColor"),
-    5: toRGB(raw.ANSIMagentaColor, "ANSIMagentaColor"),
-    6: toRGB(raw.ANSICyanColor, "ANSICyanColor"),
-    7: toRGB(raw.ANSIWhiteColor, "ANSIWhiteColor"),
-    8: toRGB(raw.ANSIBrightBlackColor, "ANSIBrightBlackColor"),
-    9: toRGB(raw.ANSIBrightRedColor, "ANSIBrightRedColor"),
-    10: toRGB(raw.ANSIBrightGreenColor, "ANSIBrightGreenColor"),
-    11: toRGB(raw.ANSIBrightYellowColor, "ANSIBrightYellowColor"),
-    12: toRGB(raw.ANSIBrightBlueColor, "ANSIBrightBlueColor"),
-    13: toRGB(raw.ANSIBrightMagentaColor, "ANSIBrightMagentaColor"),
-    14: toRGB(raw.ANSIBrightCyanColor, "ANSIBrightCyanColor"),
-    15: toRGB(raw.ANSIBrightWhiteColor, "ANSIBrightWhiteColor"),
+    0: toRGB(raw.ANSIBlackColor, "black"),
+    1: toRGB(raw.ANSIRedColor, "red"),
+    2: toRGB(raw.ANSIGreenColor, "green"),
+    3: toRGB(raw.ANSIYellowColor, "yellow"),
+    4: toRGB(raw.ANSIBlueColor, "blue"),
+    5: toRGB(raw.ANSIMagentaColor, "magenta"),
+    6: toRGB(raw.ANSICyanColor, "cyan"),
+    7: toRGB(raw.ANSIWhiteColor, "white"),
+    8: toRGB(raw.ANSIBrightBlackColor, "brightBlack"),
+    9: toRGB(raw.ANSIBrightRedColor, "brightRed"),
+    10: toRGB(raw.ANSIBrightGreenColor, "brightGreen"),
+    11: toRGB(raw.ANSIBrightYellowColor, "brightYellow"),
+    12: toRGB(raw.ANSIBrightBlueColor, "brightBlue"),
+    13: toRGB(raw.ANSIBrightMagentaColor, "brightMagenta"),
+    14: toRGB(raw.ANSIBrightCyanColor, "brightCyan"),
+    15: toRGB(raw.ANSIBrightWhiteColor, "brightWhite"),
     background: toRGB(raw.BackgroundColor, "BackgroundColor"),
     bold: toRGB(raw.TextBoldColor, "TextBoldColor"),
     cursor: toRGB(raw.CursorColor, "CursorColor"),
@@ -71,7 +85,13 @@ export function terminal(input: any): TermScheme {
 /** Convert a NSArchiver base64-encoded Buffer containing NSColor to TermSchemeColor  */
 function toRGB(archive: any, name: string): TermSchemeColor {
   if (is.undefined(archive)) {
-    throw new TypeError(`Missing ${name}`);
+    const defaultColor = terminalDefaults.colors.find((c: any) => c.name === name) || ADDITIONAL_DEFAULTS[name] as any;
+    if (!defaultColor) {
+      throw new TypeError(
+        `Missing ${name}`
+      );
+    }
+    return defaultColor.rgb;
   }
 
   if (!is.buffer(archive)) {
@@ -116,16 +136,7 @@ function toRGB(archive: any, name: string): TermSchemeColor {
   }
 
   const color = objects[1];
-
-  if (!("NSRGB" in color)) {
-    throw new TypeError(`Missing NSRGB in color ${name}`);
-  }
-
-  return color.NSRGB.toString()
-    .replace("\u0000", "")
-    .split(" ")
-    .map((item: string) => parseFloat(item))
-    .map((num: number) => Math.round(num * 255));
+  return parseNSColor(color, name);
 }
 
 function parseBplist(input: Buffer): [Error | null, any] {
@@ -133,5 +144,25 @@ function parseBplist(input: Buffer): [Error | null, any] {
     return [null, parseBuffer(input)];
   } catch (err) {
     return [err, null];
+  }
+}
+
+function parseNSColor(color: any, name: string): TermSchemeColor {
+  switch(color.NSColorSpace) {
+    case 1:
+    case 2: {
+      return color.NSRGB.toString()
+        .replace("\u0000", "")
+        .split(" ")
+        .map((item: string) => parseFloat(item))
+        .map((num: number) => Math.round(num * 255));
+    }
+    case 3: {
+      const val = parseFloat(String(color.NSWhite));
+      const result = Math.round(val * 255);
+      return [result, result, result];
+    }
+    default:
+      throw new TypeError(`Unknown NSColorSpace ${color.NSColorSpace} in color ${name}: ${util.inspect(color)}`);
   }
 }
